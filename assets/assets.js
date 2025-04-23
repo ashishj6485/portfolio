fdecfe337e261bb394e7f2f6f4bd166c751174a2
ashishj6485 <ashishj1508@gmail.com>
2025-04-24 00:46:45 +0530

    Initial commit

---
+++ assets/assets.js
@@ -0,0 +1,133 @@
+import user_image from './ashish-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import netbeans from './netbeans.png';
+import aws from './aws.png';
+import mysql from './mysql.png';
+import razorpay from './razorpay.png';
+import cloudinary from './cloudinary.png';
+import github from './github.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './ashish-logo.png';
+import logo_dark from './ashish-darklogo.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './ashish-profile.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    netbeans,
+    aws,
+    mysql,
+    razorpay,
+    cloudinary,
+    github,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Zapzy',
+        description: 'Instant Grocery',
+        bgImage: '/greencart-grocery.png',
+        link: 'https://greencart-grocery-nu.vercel.app/',
+    },
+    {
+        title: 'Connectify',
+        description: 'Web Chat App',
+        bgImage: '/chat-app.png',
+        link: 'https://github.com/ashishj6485',
+    },
+    {
+        title: 'Fixora',
+        description: 'Residential Utility Service',
+        bgImage: '/utility-service.png',
+        link: 'https://github.com/ashishj6485',
+    },
+    {
+        title: 'BankEase',
+        description: 'Banking System',
+        bgImage: '/bank-management.png',
+        link: 'https://github.com/ashishj6485',
+    },
+]
+
+export const serviceData = [
+    { icon: assets.graphics_icon, title: 'MERN Web Development', description: 'Build modern, responsive web applications using MongoDB, Express Js, React Js, and Node Js.', link: 'https://github.com/ashishj6485' },
+    { icon: assets.graphics_icon, title: 'Java Development', description: 'Develop scalable and secure applications using core and advanced Java technologies.', link: 'https://github.com/ashishj6485' },
+    { icon: assets.graphics_icon, title: 'Cloud Services', description: 'Deploy, monitor, and manage applications efficiently on cloud platforms like AWS and Azure.', link: 'https://github.com/ashishj6485' },
+    { icon: assets.graphics_icon, title: 'DevOps', description: 'Automate, scale, and streamline your development lifecycle with CI/CD, Docker, and Kubernetes.', link: 'https://github.com/ashishj6485' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Java, Python, React Js, Node Js, Express Js, C, Javascript, Tailwind CSS, HTML' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Other Skills', description: 'SpringBoot, MongoDB, MySQL, Redux, Cloudinary, Multer, RESTful APIs, Socket.io, JWT, AWS, Azure, DevOps' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.E. in Computer Science' }
+];
+
+export const toolsData = [
+    assets.vscode, assets.mongodb, assets.netbeans, assets.aws, assets.mysql, assets.razorpay, assets.cloudinary, assets.github, assets.git
+];
